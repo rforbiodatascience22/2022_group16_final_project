40d7830f6a0ec5df3a494cdf48971891ebbcb331
KongCav <s206220@dtu.dk>
2022-04-09 08:49:42 +0000

    first commit, setting up folder/file structure

---
+++ R/01_load.R
@@ -0,0 +1,27 @@
+# Load libraries ----------------------------------------------------------
+library("tidyverse")
+
+
+# Define functions --------------------------------------------------------
+source(file = "R/99_project_functions.R")
+
+
+# Load data ---------------------------------------------------------------
+my_data_raw <- read_tsv(file = "data/_raw/nhgh.tsv")
+
+
+# Wrangle data ------------------------------------------------------------
+
+# Split data
+
+
+
+# Join data
+
+
+#my_data <- my_data_raw # %>% ...
+
+
+# Write data --------------------------------------------------------------
+write_tsv(x = my_data,
+          file = "data/01_nngh.tsv")
