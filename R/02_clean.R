fe0321cc021fcf0b750cd81e6aa10ffc3250cb11
KongCav <s206220@dtu.dk>
2022-04-11 08:45:26 +0000

    drop NA from data set

--- R/02_clean.R
+++ R/02_clean.R
@@ -21,6 +21,9 @@ data <- data %>%
   mutate_at(vars(sex, re), 
             factor)
 
+data_clean <- data %>% 
+  drop_na()
+
 
 # NA treatment ------------------------------------------------------------
 
@@ -29,68 +32,68 @@ data <- data %>%
 # substitute income ranges with avg of range
 # (>= 100000 category set to 100000 as we have no other
 # way of extrapolating values in this category)
-data <- data %>% 
-  mutate(income = case_when(income == "[25000,35000)" ~ 30000,
-                            income == "[45000,55000)" ~ 50000,
-                            income == "[10000,15000)" ~ 12500,
-                            income == "[35000,45000)" ~ 40000,
-                            income == "[15000,20000)" ~ 17500,
-                            income == "[75000,100000)" ~ 87500,
-                            income == "[5000,10000)" ~ 7500,
-                            income == "[0,5000)" ~ 2500,
-                            income == "[20000,25000)" ~ 22500,
-                            income == ">= 100000" ~ 100000,
-                            income == "[65000,75000)" ~ 70000,
-                            income == "[55000,65000)" ~ 60000,
-                            income == "< 20000" ~ 1, 
-                            income == "> 20000" ~ 2))
+#data <- data %>% 
+#  mutate(income = case_when(income == "[25000,35000)" ~ 30000,
+#                            income == "[45000,55000)" ~ 50000,
+#                            income == "[10000,15000)" ~ 12500,
+#                            income == "[35000,45000)" ~ 40000,
+#                            income == "[15000,20000)" ~ 17500,
+#                            income == "[75000,100000)" ~ 87500,
+#                            income == "[5000,10000)" ~ 7500,
+#                            income == "[0,5000)" ~ 2500,
+#                            income == "[20000,25000)" ~ 22500,
+#                            income == ">= 100000" ~ 100000,
+#                            income == "[65000,75000)" ~ 70000,
+#                            income == "[55000,65000)" ~ 60000,
+#                            income == "< 20000" ~ 1, 
+#                            income == "> 20000" ~ 2))
 
 # calculate replacement for <20000 category by
 # distribution of the 0-5000, 5000-10000, 10000-15000
 # and 15000-20000 income ranges
-sub_20000 <- data %>% 
-  group_by(income) %>% 
-  filter(income == 2500 | 
-           income == 7500 | 
-           income == 12500 | 
-           income == 17500 ) %>% 
-  summarise(n_distinct(seqn)) %>% 
-  mutate(incomeXn = income * `n_distinct(seqn)`) %>% 
-  summarise(sum(incomeXn)/sum(`n_distinct(seqn)`)) %>% 
-  pull()
+#sub_20000 <- data %>% 
+#  group_by(income) %>% 
+#  filter(income == 2500 | 
+#           income == 7500 | 
+#           income == 12500 | 
+#           income == 17500 ) %>% 
+#  summarise(n_distinct(seqn)) %>% 
+#  mutate(incomeXn = income * `n_distinct(seqn)`) %>% 
+#  summarise(sum(incomeXn)/sum(`n_distinct(seqn)`)) %>% 
+#  pull()
 
 # calculate replacement for >20000 category by same idea
-over_20000 <- data %>% 
-  group_by(income) %>% 
-  filter(income == 22500 | 
-           income == 30000 | 
-           income == 40000 | 
-           income == 50000 |
-           income == 60000 |
-           income == 70000 |
-           income == 87500 |
-           income == 100000) %>% 
-  summarise(n_distinct(seqn)) %>% 
-  mutate(incomeXn = income * `n_distinct(seqn)`) %>% 
-  summarise(sum(incomeXn)/sum(`n_distinct(seqn)`)) %>% 
-  pull()
+#over_20000 <- data %>% 
+#  group_by(income) %>% 
+#  filter(income == 22500 | 
+#           income == 30000 | 
+#           income == 40000 | 
+#           income == 50000 |
+#           income == 60000 |
+#           income == 70000 |
+#           income == 87500 |
+#           income == 100000) %>% 
+#  summarise(n_distinct(seqn)) %>% 
+#  mutate(incomeXn = income * `n_distinct(seqn)`) %>% 
+#  summarise(sum(incomeXn)/sum(`n_distinct(seqn)`)) %>% 
+#  pull()
 
 # replace >20000 and <20000 categories with estimates
-data <- data %>% 
-  mutate(income = case_when(income == 1 ~ sub_20000,
-                            income == 2 ~ over_20000,
-                            TRUE ~ as.numeric(as.character(income))))
+#data <- data %>% 
+#  mutate(income = case_when(income == 1 ~ sub_20000,
+#                            income == 2 ~ over_20000,
+#                            TRUE ~ as.numeric(as.character(income))))
 
 
 # calculate avg income (without NA observations)
-avg_income <- data %>%
-  drop_na(income) %>% 
-  summarise(avg_income = mean(income)) %>% 
-  pull()
+#avg_income <- data %>%
+#  drop_na(income) %>% 
+#  summarise(avg_income = mean(income)) %>% 
+#  pull()
 
 # substitute income NAs with avg income
-data <- data %>% 
-  replace_na(list(income = avg_income))
+#data <- data %>% 
+#  replace_na(list(income = avg_income))
 
 # Binarization of gender --------------------------------------------------
 #data %>% mutate(sex_binarized = case_when(sex == 'female' ~ -1, 
