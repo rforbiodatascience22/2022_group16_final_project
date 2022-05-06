0685d0f52d24b71063d754ac0cfb5d60d7590ddb
KongCav <s206220@dtu.dk>
2022-05-06 10:52:36 +0000

    working on packagechecker

---
+++ R/98_check_packages.R
@@ -0,0 +1,17 @@
+# List of used packages
+packages_used = c("tidyverse",
+                  "pdist",
+                  "broom",
+                  "ggplot2",
+                  "patchwork",
+                  "scales",
+                  "ggridges",
+                  "corrplot",
+                  "stringr",
+                  "glmnet",
+                  "corrr")
+
+for(p in packages_used){
+  if(!require(p,character.only = TRUE)) install.packages(p)
+  library(p,character.only = TRUE)
+}
