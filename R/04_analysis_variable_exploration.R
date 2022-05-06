dbf5038026aec49861cd5520003c47c9a4d7594a
Linesandvad <s184339@student.dtu.dk>
2022-05-06 08:08:58 +0000

    change 04

--- R/04_analysis_variable_exploration.R
+++ R/04_analysis_variable_exploration.R
@@ -1,7 +1,7 @@
 # Load libraries ----------------------------------------------------------
 library(tidyverse)
 library(ggplot2)
-library(gridExtra)
+#library(gridExtra)
 library(patchwork)
 library(scales)
 library(ggridges)
@@ -19,34 +19,46 @@ data <- read_tsv(file = "data/03_nhgh_clean_aug.tsv")
 
 ##Linear correlations between each variable, presented
 #using corrplot
-corr <- data %>% 
-  select(-c(seqn, gender, re, tx, dx, `BMI class`, Treatment_number)) %>%
+corr_matrix <- data %>%
+  select(-c(seqn, 
+            gender, 
+            re, 
+            tx, 
+            dx, 
+            `BMI class`, 
+            Treatment_number)) %>%
   select_if(is.numeric) %>% 
-  cor( method = "pearson", 
-       use = "complete.obs" )
+  cor(method = "pearson",
+      use = "complete.obs")
 
-corrplot(corr, type = "upper" , order = "hclust", 
-         tl.col = "black", tl.srt = 45, tl.cex = 1)
+corrplot(corr_matrix, 
+         type = "upper", 
+         order = "hclust", 
+         tl.col = "black",
+         tl.srt = 45,      
+         tl.cex = 1)
 
 
-##Violin plot (and boxplots) of income, filtered on ethnicity 
+##Violin plot (and boxplots) of income, sorted on ethnicity 
 data %>% 
-  ggplot(aes(x = re, 
-             y = income)) + 
+  ggplot(mapping = aes(x = re, 
+                       y = income)) + 
   geom_boxplot(outlier.color = "red", 
                outlier.shape = 1, 
                outlier.size = 2) + 
   geom_violin(alpha = 1/10, 
               color = "grey") +  
   theme(panel.grid.major = element_blank(),
-        axis.text.x = element_text(angle = 20, size = 7)) + 
-  labs(x = " ", 
+        axis.text.x = element_text(angle = 20, 
+                                   size = 7)) + 
+  labs(x = "", 
        y = "Annual income", 
        title = "Annual income by ethnicity", 
-       caption = "red dot = outlier", size = 2)
+       caption = "Red dot = outlier", 
+       size = 2)
 
 
-
+#Distribution of individuals' BMI in the dataset 
 bmi_cols <- data %>%  
   count(`BMI class`) %>% 
   drop_na(`BMI class`) %>% 
@@ -60,13 +72,16 @@ bmi_cols <- data %>%
                                    "Morbid obesity",
                                    "Super obese"))) %>% 
   ggplot(mapping = aes(x = `BMI class`, 
-             y = Percentage)) +
+                       y = Percentage,
+                       fill = `BMI class`)) +
   geom_col() + 
   labs(x = "BMI class", 
-       title = "Distribution of BMI classification") +
-  theme(axis.text.x = element_text(angle = 15))
+       title = "Distribution of BMI classes") +
+  theme(axis.text.x = element_text(angle = 15),
+        legend.position = "none")
 
 
+#Boxplots showing BMI class vs. age, stratified on diagnose status
 bmi_boxplots <- data %>% 
   mutate(dx_class = case_when(dx == 0 ~ "Not diagnosed", 
                               dx == 1 ~ "Diagnosed")) %>% 
@@ -78,9 +93,9 @@ bmi_boxplots <- data %>%
                                   "Severe obesity",
                                   "Morbid obesity",
                                   "Super obese"))) %>% 
-  ggplot(aes(x = `BMI class`, 
-             y = age, 
-             fill = dx_class)) + 
+  ggplot(mapping = aes(x = `BMI class`, 
+                       y = age, 
+                       fill = dx_class)) + 
   geom_boxplot() + 
   theme(legend.position = "bottom",
         axis.text.x = element_text(angle = 15)) +
@@ -89,7 +104,9 @@ bmi_boxplots <- data %>%
         y = "Age",
        title = "BMI class and age in relation to diagnosis")
 
-bmi_cols / bmi_boxplots
+#Use patchwork to combine the two plots
+(bmi_cols / bmi_boxplots) + plot_annotation(
+  caption = "BMI classes classified are as defined in the exercise of week 4.")
 
 
 
@@ -97,12 +114,16 @@ bmi_cols / bmi_boxplots
 data %>%  
   mutate(sex_class = case_when(gender == "female" ~ 0,
                                gender == "male" ~ 1),
+         Gender = case_when(gender == "female" ~ "Female",
+                            gender == "male" ~ "Male"),
          bfp = 1.39 * bmi + 0.16 * age - 10.34 * sex_class - 9) %>% 
-  ggplot(aes(x = bfp,
-             y = stat(density),
-             color = gender)) + 
+  ggplot(mapping = aes(x = bfp,
+                       y = stat(density),
+                       color = Gender)) + 
   geom_freqpoly(binwidth = 2) + 
   facet_wrap(~ re) +
   theme(legend.position = "bottom") + 
   labs(x = " Body fat percentage", 
-       title = "Sex-based distribution of body fat percentage")
+       y = "Density",
+       title = "Gender-based distribution of body fat percentage",
+       subtitle = "Divided based on ethnicity")
