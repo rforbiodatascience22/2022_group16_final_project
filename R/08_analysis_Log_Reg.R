714fad04b72cd95f30616beb4b4b408a3f352d4f
Linesandvad <s184339@student.dtu.dk>
2022-05-07 09:47:41 +0000

    Log reg cleaning and figure generation

--- R/08_analysis_Log_Reg.R
+++ R/08_analysis_Log_Reg.R
@@ -1,10 +1,13 @@
 # Load libraries ----------------------------------------------------------
 library("tidyverse")
 library("ggplot2")
+library("dplyr")
+library("purrr")
 library("glmnet")
+library("tidymodels")
+library("patchwork")
 library("corrr")
 library("broom")
-
 
 # Define functions --------------------------------------------------------
 source(file = "R/99_project_functions.R")
@@ -21,8 +24,6 @@ my_data_clean_aug <- my_data_clean_aug %>%
   select_if(is.numeric)
 
 
-
-
 #Creating a co-relation matrix We use this to try and identify which variables 
 # to avoid due to co-linearity
 
@@ -33,13 +34,13 @@ corr_matrix <- my_data_clean_aug %>%
 # Multi-factor Model -------------------------------------------------------
 ## DATA 1 factors are randomly chosen
 
-data1 <- glm(dx ~ age +
-               bmi +
-               male + 
-               female,
-             data = my_data_clean_aug,
-             family = binomial(link = "logit"))
-summary(data1)
+glm_rand <- glm(dx ~ age +
+                  bmi +
+                  male + 
+                  female,
+                data = my_data_clean_aug,
+                family = binomial(link = "logit"))
+#summary(glm_rand)
 
 ### Data mlm with race 
 multi_with_race <- glm(dx ~ age +
@@ -61,7 +62,6 @@ multi_with_race <- glm(dx ~ age +
 # the result of which we will use to identify which variables have an impact on
 # the diabetes value and whether that impact evaluation is statistically 
 #significant or not
-
 
 data_visual <- my_data_clean_aug %>% 
   select(-tx,
@@ -101,11 +101,11 @@ data_visual <- my_data_clean_aug %>%
 ###  Variable impact estimation with confidence interval 
 
 data_visual %>% 
-  filter(id != "single_model1_fitted",
+  filter(id != "single_bmi_fitted",
          id != "mult_race_fitted", 
-         id != "model1_fited", 
-         id != "mult_model4_fitted", 
-         id != "mlm_v1",
+         id != "model_rand_fitted", 
+         id != "mlm_confident", 
+         id != "mlm_improved",
          id != "single_gh_fitted") %>% 
   ggplot(aes(x = estimate,
              y = fct_reorder(id,
@@ -127,17 +127,21 @@ data_visual %>%
   theme(axis.text.y = element_blank(),
         legend.position = "bottom") +
   labs(color = "Identified As", 
-       y = "Parameter type ",
-       x = "Estimated Impact on Diagnosis") 
+       y = "Parameter",
+       x = "Estimated Impact on Diagnosis",
+       title = "Impact Estimation with confidence interval")
+ggsave("./results/Impact_Estimation_with_confidence_interval.png")
+
+
 
 ### Manhattan Plot for identifying variables that have significant P Values   
 
 data_visual %>%
-  filter(id != "single_model1_fitted",
+  filter(id != "single_bmi_fitted",
          id != "mult_race_fitted",
-         id != "model1_fited",
-         id != "mult_model4_fitted",
-         id != "mlm_v1",
+         id != "model_rand_fitted",
+         id != "mlm_confident",
+         id != "mlm_improved",
          id != "single_gh_fitted") %>% 
   ggplot(mapping = aes(x = id, 
                        y = neg_log10_p,
@@ -155,40 +159,49 @@ data_visual %>%
                 base_size = 8) + 
   theme(axis.text.x = element_blank(), 
         legend.position = "bottom") + 
-  labs(caption = "------ Negative log value is 0.05" , colour = "Identified As",
-       x = "The Different Parameters of the Dataset",
-       y = "Minus log10(p)")
+  labs(caption = "------ Negative log value is 0.05" , 
+       colour = "Identified As",
+       x = "Parameters",
+       y = "Minus log10(p)",
+       title = "Manhattan plot",
+       subtitle = "Identification of parameters with significant P-values")
+ggsave("./results/Parameters_vs_p_value.png")
+
 
 # Single Factor Logistic Regression
 
-
-single_bmi <-  glm(dx ~ bmi,
-                   data = my_data_clean_aug,
-                   family = binomial(link = "logit"))
+single_bmi <- glm(dx ~ bmi,
+                  data = my_data_clean_aug,
+                  family = binomial(link = "logit"))
 
 single_gh <- glm (dx ~ gh,
                   data = my_data_clean_aug,
                   family = binomial(link = "logit"))
 
-summary(single_bmi)
-
-summary(single_gh)
-
+#summary(single_bmi)
+#summary(single_gh)
 
 
-
-mult_improved1 <- glm(dx ~ age +bmi +gh+albumin,
-                      data = my_data_clean_aug,
-                      family = binomial(link = "logit"))
+mult_improved <- glm(dx ~ age +
+                       bmi +
+                       gh +
+                       albumin,
+                     data = my_data_clean_aug,
+                     family = binomial(link = "logit"))
 
 #improved algorithm after checking for co-linearity and singular value estimation
-mlm_after_colinearity_check <- glm(dx ~ age +bmi +gh+albumin+SCr+bun,
+mlm_after_colinearity_check <- glm(dx ~ age +
+                                     bmi +
+                                     gh +
+                                     albumin +
+                                     SCr + 
+                                     bun,
                                    data = my_data_clean_aug,
                                    family = binomial(link = "logit"))
 
-summary(mlm_after_colinearity_check)
-summary(multi_with_race)
-summary(mult_improved1)
+#summary(mlm_after_colinearity_check)
+#summary(multi_with_race)
+#summary(mult_improved)
 
 
 ### code for making a new variable that is a prediction on whether a person is 
@@ -197,21 +210,25 @@ summary(mult_improved1)
 
 #Adding the fitted values from our different values to the main dataset
 my_data_clean_aug <- my_data_clean_aug %>%
-  
-  mutate(mult_race_fitted = multi_with_race$fitted.values, model1_fited = data1$fitted.values,
-         mlm_v1= mult_improved1$fitted.values,single_model1_fitted =single_bmi$fitted.values,
+  mutate(mult_race_fitted = multi_with_race$fitted.values, 
+         model_rand_fitted = glm_rand$fitted.values,
+         mlm_improved = mult_improved$fitted.values,
+         single_bmi_fitted = single_bmi$fitted.values,
          single_gh_fitted = single_gh$fitted.values,
-         mult_model4_fitted = mlm_after_colinearity_check$fitted.values)
+         mlm_confident = mlm_after_colinearity_check$fitted.values) %>% 
+  mutate(`Diagnose status` = 
+           case_when(dx == 0 ~ "Not diagnosed",
+                     dx == 1 ~ "Diagnosed"))
 
 
 ### Sorting the dataset by 
+mlm_prediction_vs_dx <- my_data_clean_aug %>% 
+  arrange(desc(mlm_improved)) %>% 
+  mutate(rank_of_mlm_improved = 1:nrow(my_data_clean_aug))
 
-mlm_prediction1_vs_dx <- my_data_clean_aug %>% 
-  arrange(desc(mlm_v1)) %>% 
-  mutate(rank_of_mlm_v1 = 1:nrow(my_data_clean_aug))
 
 mlm_after_colinearity_check_srt <- my_data_clean_aug %>% 
-  arrange(desc(mult_model4_fitted)) %>% 
+  arrange(desc(mlm_confident)) %>% 
   mutate(rank_of_mult4 = 1:nrow(my_data_clean_aug))
 
 
@@ -219,8 +236,9 @@ data_gh_single <- my_data_clean_aug %>%
   arrange(desc(single_gh_fitted)) %>% 
   mutate(rank_of_single = 1:nrow(my_data_clean_aug))
 
+
 data_bmi_single <- my_data_clean_aug %>%
-  arrange(desc(single_model1_fitted)) %>% 
+  arrange(desc(single_bmi_fitted)) %>% 
   mutate(rank_of_bmi_pred = 1:nrow(my_data_clean_aug))
 
 
@@ -228,47 +246,92 @@ data_bmi_single <- my_data_clean_aug %>%
 ### of predicted vs actual presence of diabetes
 
 data_bmi_single <- data_bmi_single %>% 
-  mutate(predicted_single_bmi = case_when(single_model1_fitted > 0.35 ~1,
-                                          single_model1_fitted <= 0.35 ~ 0))
+  mutate(predicted_single_bmi = 
+           case_when(single_bmi_fitted > 0.35 ~1,
+                     single_bmi_fitted <= 0.35 ~ 0))
 
-mlm_prediction1_vs_dx <- mlm_prediction1_vs_dx %>% 
-  mutate(predicted_mult3= case_when(mlm_v1 > 0.35 ~ 1,
-                                    mlm_v1 <= 0.35 ~ 0))
+mlm_prediction_vs_dx <- mlm_prediction_vs_dx %>% 
+  mutate(predicted_mult3 = 
+           case_when(mlm_improved > 0.35 ~ 1,
+                     mlm_improved <= 0.35 ~ 0))
 
 mlm_after_colinearity_check_srt <- mlm_after_colinearity_check_srt %>% 
-  mutate(predicted_mult4= case_when(mult_model4_fitted > 0.35 ~ 1,
-                                    mult_model4_fitted <= 0.35 ~ 0))
+  mutate(predicted_mult4 = 
+           case_when(mlm_confident > 0.35 ~ 1,
+                     mlm_confident <= 0.35 ~ 0))
+
 data_gh_single <- data_gh_single %>% 
-  mutate(predicted_single= case_when(single_gh_fitted > 0.35 ~ 1,
-                                     single_gh_fitted <= 0.35 ~ 0))
-
-
+  mutate(predicted_single = 
+           case_when(single_gh_fitted > 0.35 ~ 1,
+                     single_gh_fitted <= 0.35 ~ 0))
 
 
 ### trying to plot probability of getting diagnosed with diabetes v/s 
 ## actually having diabetes
 
-ggplot(data=mlm_prediction1_vs_dx , mapping =  aes(x=rank_of_mlm_v1, y=mlm_v1)) +
-  geom_point( mapping = aes(color=as.factor(dx)), alpha=1, shape=4,stroke=2)+
-  labs(y = " Prediction based on only colinearity check ")
+ggplot(data = mlm_prediction_vs_dx, 
+       mapping = aes(x = rank_of_mlm_improved, 
+                     y = mlm_improved,
+                     color = `Diagnose status`)) +
+  geom_point(alpha=1, 
+             shape=4,
+             stroke=2)+
+  labs(y = "Prediction",
+       x = "Index",
+       title = "Logisitic regression model prediction",
+       subtitle = "Based on co-linearity matrix") +
+  theme(legend.position = "bottom")
+ggsave("./results/Prediction_on_colinearity_check.png")
 
-ggplot(data=mlm_after_colinearity_check_srt , mapping =  aes(x=rank_of_mult4, y=mult_model4_fitted)) +
-  geom_point( mapping = aes(color=as.factor(dx)), alpha=1, shape=4,stroke=2) +
-  labs( y = "Prediction based on model after Colinearity check and p-values")
-
-ggplot(data=data_gh_single ,  mapping = aes(x=rank_of_single, y=single_gh_fitted)) +
-  geom_point( mapping = aes(color=as.factor(dx)), alpha=1, shape=4,stroke=2)+
-  labs( y = " Prediction based only GH value")
-
-ggplot(data=data_bmi_single ,  mapping = aes(x=rank_of_bmi_pred, y=single_model1_fitted)) +
-  geom_point( aes(color=as.factor(dx)), alpha=1, shape=4,stroke=2)+
-  labs( y = "Prediction based on BMI Value")
-
-
-
+ggplot(data = mlm_after_colinearity_check_srt, 
+       mapping = aes(x = rank_of_mult4, 
+                     y = mlm_confident,
+                     color = `Diagnose status`)) +
+  geom_point(alpha = 1, 
+             shape = 4,
+             stroke = 2) +
+  labs( y = "Prediction",
+        x = "Index",
+        title = "Multi-factor Logistic regression model prediction",
+        subtitle = "Based on model after co-linearity check and p-values")
+ggsave("./results/multi_factor_colinearity_pval.png")
 
 
-xtabs(~ dx + predicted_mult3, data=mlm_prediction1_vs_dx)
-xtabs (~ dx + predicted_mult4, data=mlm_after_colinearity_check_srt)
-xtabs (~ dx + predicted_single, data =data_gh_single)
-xtabs(~ dx + predicted_single_bmi, data = data_bmi_single )
+p4 <- ggplot(data = data_gh_single, 
+             mapping = aes(x = rank_of_single, 
+                           y = single_gh_fitted,
+                           color = `Diagnose status`)) +
+  geom_point(alpha = 1, 
+             shape = 4,
+             stroke = 2)+
+  labs( y = "Prediction",
+        x = "Index",
+        title = "Glycohemoglobin")+
+  theme(legend.position = "right")
+
+
+p3 <- ggplot(data = data_bmi_single,
+             mapping = aes(x = rank_of_bmi_pred, 
+                           y = single_bmi_fitted,
+                           color = `Diagnose status`)) +
+  geom_point(alpha = 1, 
+             shape = 4,
+             stroke = 2, 
+             show.legend = FALSE)+
+  labs(x = "Index",
+       y = "Prediction",
+       title = "BMI")
+
+p3 + p4 +
+  plot_annotation(title = "Single-factor logistic regression models",
+                  subtitle = "Prediction based on")
+
+ggsave("./results/Single_Prediction.png")
+
+
+#xtabs(~ dx + predicted_mult3, data=mlm_prediction_vs_dx)
+#xtabs (~ dx + predicted_mult4, data=mlm_after_colinearity_check_srt)
+#xtabs (~ dx + predicted_single, data =data_gh_single)
+#xtabs(~ dx + predicted_single_bmi, data = data_bmi_single )
+
+
